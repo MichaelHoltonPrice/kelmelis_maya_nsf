5bbdd6c78c5494e54c403d000718e7682157baa5
MichaelHoltonPrice <MichaelHoltonPrice@gmail.com>
2023-12-19 11:05:23 -0800

    Added a detailed description of the power calculation at the top of do_power_analysis.R

--- do_power_analysis.R
+++ do_power_analysis.R
@@ -1,3 +1,152 @@
+# This script runs the power analysis for the NSF proposal by Kelmelis et al.
+# named:
+#
+# A new end-to-end Bayesian and osteological approach to reconstructing
+# demographic changes for the ancient Maya
+#
+# DEMOGRAPHIC MODEL
+#
+# The target of the power analysis is a simuluated boost to mortality during
+# famine years of kappa = 0.2. We assume a baseline Siler mortality hazard of
+#
+# haz_good = a_1 * exp(-a_2 * x) + a_3 + a_4 * exp(a_5 * x)
+#
+# where we use a0 <- c(0.175,1.40,0.368*0.01,0.075*0.001,0.917*0.1) for
+# the simulated, baseline hazard. This baseline hazard is for good/non-famine
+# years. During a famine, the hazard is increased by (1 + kappa) (20% higher)
+#
+# haz_bad = (1 + kappa) * haz_good
+#
+# To fully specify the simulated demography we must also define a fertility
+# model. We use a simple model where (a) age-specific, instantaneous fertility
+# is constant from ages 15 to 45 and 0 otherwise and (b) the total fertility
+# rate (TFR) is 5. For details (e.g., what precisely we mean by instantaneous
+# fertility), see the documentation for the function calc_F in the file
+# power_analysis_functions.R.
+#
+# HIDDEN MARKOV MODEL
+#
+# The other major component of the model is a hidden Markov model that
+# determines which years are famine years and which are non-famine years.
+# This requires just one parameter, p_gb, which is the probability of
+# transitioning from a non-famine to a famine year (_gb in p_gb marks that this
+# is the probability of going from a good to bad year). We further assume
+# that the probability of going from a bad year to another bad year is 2*p_gb.
+# That is, once a bad year is reached one gets "stuck" in it. This adds some
+# realism since there will, on average, be clusters of good and bad years. The
+# transition matrix, which we usually call H in the code, is thus (applying
+# conversation of probabilities):
+#
+# [1 -   p_gb,   p_gb]
+# [1 - 2*p_gb, 2*p_gb]
+#
+# Broken out and using some English, the trasitions are governed by:
+#
+# non-famine -->     famine   p_gb
+# non-famine --> non-famine   1 - p_gb
+#     famine -->     famine   2*p_gb
+#     famine --> non-famine   1 - 2*p_gb
+#
+# Associated with these transition probabilities are actual, realized
+# sequences of good and bad years. In the code, we call these realized sequences
+# h or famine_mask (a boolean vector). For example, for each
+# experiment/simulation that contributes to the power calculation there is a
+# true, realized famine_mask. If the mask begins [FALSE, FALSE, TRUE,...], then
+# the first two years (AD 800 and 801) are good/non-famine years and the third
+# year (AD 802) is a bad/famine year. Given a realized famine mask sequence h
+# and the demographic parameter vector theta = [a, kappa, TFR], one can fully
+# calculate the realized demography, which is a matrix indexed by year and age,
+# where each entry in the matrix gives the probability of dying in that given
+# year at that given age (at least, this is the right conceptual mindset; the
+# details matter and the code takes advantage of the fact that we actually only
+# need the marginal probabilities). For details, see the documentation for the
+# function calc_pop_size in the file power_analysis_functions.R.
+#
+# POWER CALCULATION
+#
+# The target of the power calculation, as already noted above, is the parameter
+# kappa, which is the "boost" to mortality during bad/famine years. For each
+# value of the sample size, N, we run 400 simulated experiments. An experiment
+# passes if the 95% highest density interval (HDI) from Bayesian samples of the
+# estimated value of kappa falls entirely within the region of practical
+# equivalence (ROPE). It makes sense that we require the reconstructed kappa
+# to be positive (since we want to assess whether we can correctly identify
+# boosts to mortality that results from famine years). Hence, we use 0 as the
+# lower limit of the ROPE. To make things symmetric, we define the upper limit
+# of the ROPE to be 0.4, so that values of the HDI between 0.2 +/- 0.2 qualify
+# as a success.
+#
+# The other major parameter in the power calculation is a flag called use_age.
+# If use_age is TRUE, the reconstruction may use simulated skeletal
+# age-at-death estimates. We assume fewer estimates of this type are available
+# compared to radiocarbon dates. In particular, we use 10% of the radiocarbon
+# sample size for the skeletal sample size, rounded to the nearest integer. For
+# example, if there are 2000 radiocarbon samples there are 200 skeletal samples.
+#
+# Ultimately, the power calculation yields two curves: the success probability
+# as a function of sample size both with and without skeletal samples.
+#
+# EXPERIMENTS
+# 
+# Each experiment consists of the following steps:
+#
+# (1) First, create simulated data. This uses the true/simulated values for
+#     a, kappa, TFR, and p_gb. The simulated dataset includes a true/realized
+#     sequence of good and bad years, true/realized sample calendar dates
+#     and ages-at-death, and realized radiocarbon measurements that account
+#     for the radiocarbon calibration curve and measurement uncertainty.
+#
+# (2) Second, do posterior Bayesian sampling of the parameters a, kappa, TFR,
+#     and p_gb. Crucially, only quantities available in practice are used for
+#     this Bayesian inference; for example, the realized sequence of good and
+#     bad years is not used and only the radiocarbon measurements are
+#     available, not the realized calendar dates. In addition to sampling the
+#     preceding parameters, we also sample the estimated famine mask. Below we
+#     provide further details on the posterior Bayesian sampling.
+#
+# (3) Third, aggregate and plot the experimental results. An experiment passes
+#     if the HDI for the posterior samples of kappa lies fully within the
+#     ROPE, which spans 0 to 0.4.
+#
+# BAYESIAN SAMPLING
+#
+# We utilize an iterative sampling with three steps involving the following
+# parameters:
+#
+#  theta = th = [a_1, a_2, a_3, a_4, a_5, kappa, TFR]
+#  m          = [p_gb]
+#  h          = The estimated famine mask, or sequence of good and bad years
+#
+# Step 1: Sample h|m (h given m)
+#
+# The parameter p_gb completely specifies the Hidden markov model for good and
+# bad years. We draw a novel sample of the realized sequence, h, each
+# iteration. Even though m consists only of p_gb, we utilize a distinct symbol
+# for the hidden Markov parameter(s) because future models will almost certainly
+# be more sophisticated and involve more parameters. Nevertheless, the sampling
+# approach remains the same.
+#
+# Step 2: Sample th|h (theta given h)
+#
+# Given a realized famine mask, we can calculate the likelihood, or probability,
+# of the parameter vector th. At each step, we propose a new value, th_prop,
+# by making a Gaussian draw. We utilize a conventional Metropolis-Hastings
+# accept/reject step based on the ratio of the likelihoods. If the proposed
+# parameter vector is accepted, we replace th with th_prop; otherwise, we keep
+# the original value of th. In this preliminary model, we do not account for
+# prior probabilities on the parameter values, which amounts to assigning
+# uniform priors.
+#
+# Step 3: Sample m|h (m given h)
+#
+# Given a realized famine mask, we calculate the likelihood, or probability,
+# of the parameter "vector" m. As in Step 2, we make a Gaussian draw to propose
+# a new value of m, m_prop, and utilize an accept/reject step based on the
+# ratio of likelihoods.
+#
+# We repeat the preceding steps 2000 times, and use only the final 1000 values
+# for the HDI calculation (i.e., we adopt a "burn-in" period of 1000 samples).
+
 # 0. Clear the workspace, load packages, source the needed functions, create a
 #    data directory (if necessary), and create an analysis name based on the
 #    current time.
@@ -58,7 +207,7 @@ start_date <- 600 # AD 600
 # Define the vector of calendar dates for the study period
 tau <- seq(start_date, start_date + num_times -1)
 
-# N_vect is the vector of samples sizes. The power calculation involves
+# N_vect is the vector of sample sizes. The power calculation involves
 # determing the number of times each experiment/simulation succeeds for
 # each value of the sample size. See the method exp_wrapper in
 # power_analysis_functions.R for what constitutes success. For the
