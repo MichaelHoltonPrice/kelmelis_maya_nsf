5a6156d0746b7aa971f4ebe0efed795f18cab20a
MichaelHoltonPrice <MichaelHoltonPrice@gmail.com>
2023-12-18 17:27:53 -0800

    Improved the comments and documentations in do_power_analysis.R

--- do_power_analysis.R
+++ do_power_analysis.R
@@ -15,6 +15,7 @@ if (!dir.exists('data')) {
 
 analysis_name <- as.character(Sys.time())
 analysis_name <- gsub(":", "-", analysis_name)
+
 # 1. Set the true values
 
 # The true total fertiltiy rate is 5 (half of babies are female)
@@ -28,57 +29,54 @@ kappa0 <- .2
 
 # The final model parameter is the probability of transitioning from a
 # non-famine to a famine year, which is half the probabilty of transitioning
-# from a famine to a famine year. This parameter is the effect size used in
-# the power analysis, and hence is not set here.
-
-# For reproducibility, set the random number
-set.seed(1000)
-
-# 2. Iterate over effect size to calculate the power (0.025 to 0.4 by .025)
-
-# p_gb is the effect size and stands for the probability of good to bad
-# (non-famine to famine year). That is, we assume a hidden markov model for the
-# un-observed famine state (that is, not directly observed; it is indirectly
-# inferred), where the transition matrix is
+# from a famine to a famine year. We call this parameter p_gb (or p_gb0 for the
+# true, simulated value). The transition matrix is
 #
 # [1 -   p_gb,   p_gb]
 # [1 - 2*p_gb, 2*p_gb]
 #
-# and the first state is the good (non-famine) state. That is, the individual
-# transition probabilities are
+# where the first state is the good (non-famine) state. Put slightly differently,
+# the individual transition probabilities are
 #
 # non-famine -->     famine   p_gb
 # non-famine --> non-famine   1 - p_gb
 #     famine -->     famine   2*p_gb
 #     famine --> non-famine   1 - 2*p_gb
+p_gb <- .2 # TODO: this should be called p_gb0 for consistency with the other true values
 
-
-
-# First, define a function to calculate the population size given the input
-# parameter vectors, including the realized famine years. To do so, utilize a
-# discrete, age-structured population with one year age intervals and one year
-# time steps.
-
+# 2. Run the experiments/simulations
+# Load the intcal20 calibration curve and define the error model
+# for radiocarbon samples
 calib_df <- load_calib_curve('intcal20')
 error_spec=list(type="unif_fm",min=.0021,max=.0028)
 
+# Our study period is 400 years, and each time period is one year,
+# so there are 400 time periods
 num_times <- 400
-start_date <- 600 # AD 600 is the first date
+# Our simulated study period starts in AD 600
+start_date <- 600 # AD 600
+# Define the vector of calendar dates for the study period
 tau <- seq(start_date, start_date + num_times -1)
-# The number of experiments for each value of the effect size
 
-
-p_gb <- .2
+# N_vect is the vector of samples sizes. The power calculation involves
+# determing the number of times each experiment/simulation succeeds for
+# each value of the sample size. See the method exp_wrapper in
+# power_analysis_functions.R for what constitutes success. For the
+# simulations that include age-at-death observations, we utilize N/10
+# skeletons with an age-at-death estimate for every radiocarbon samples.
 N_vect <- 100*2^(0:5)
-N_vect <- N_vect[5:6]
-#N_vect <- N_vect[3:5]
-#N_vect <- c(100,1000)
-#exp_per_N <- 100
+
+# We run 400 simulations for each value of the sample size so that we
+# can estimate the success probability.
 exp_per_N <- 400
 
+# Set seeds for reproducibility
 set.seed(1011)
 seed_matrix <- matrix(sample.int(length(N_vect)*exp_per_N),nrow=length(N_vect))
 
+# Loop over experiments to create, for each, the problem needing to be solved.
+# This involves three nested for loops: (a) samples sizes in N_vect, (b) 400
+# experiments per sample size, and (c) with and without age-at-death observations.
 counter <- 0
 prob_list <- list()
 for (k1 in 1:length(N_vect)) {
@@ -106,6 +104,7 @@ for (k1 in 1:length(N_vect)) {
 
 t0 <- Sys.time()
 
+# Call the wrapper that runs experiments inside a parallel for loop
 package_list <- c('baydem', 'matrixStats', 'HDInterval', 'doParallel')
 registerDoParallel(detectCores())
 t0 <- Sys.time()
@@ -115,7 +114,8 @@ success_vect <- foreach(n=1:length(prob_list),.combine=cbind, .packages=package_
 t1 <- Sys.time()
 stopImplicitCluster()
 
-# For clarity, use another set of for loops to unpack success_vect
+# 3. Plots the results
+# Now that all experiments have finished, extract the success metrics. 
 success_array <- array(NA,dim=c(length(N_vect), exp_per_N, 2))
 counter <- 0
 for (k1 in 1:length(N_vect)) {
@@ -128,6 +128,7 @@ for (k1 in 1:length(N_vect)) {
   }
 }
 
+# Plot the success probabilities (statistical power)
 power_vect_no_age <- rowSums(success_array[,,1]) / ncol(success_array[,,1])
 power_vect_age    <- rowSums(success_array[,,2]) / ncol(success_array[,,2])
 
