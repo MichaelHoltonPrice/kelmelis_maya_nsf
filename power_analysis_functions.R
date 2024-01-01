588cc28630c7d00a4364ef0151d095e1671217a4
MichaelHoltonPrice <MichaelHoltonPrice@gmail.com>
2023-12-31 17:23:14 -0800

    Improved Figure 2

--- power_analysis_functions.R
+++ power_analysis_functions.R
@@ -785,7 +785,7 @@ plot_analysis <- function(analysis_name, N_vect, exp_per_N) {
   # Make the actual plot, which has two x-axes: one for radiocarbon dates (N)
   # and the other for skeletal samples (round(N/10)).
   y_max <- max(power_vect_no_age, power_vect_age, na.rm=TRUE)
-  pdf(paste0('power_curve_',analysis_name,'.pdf'))
+  png(paste0('Fig2_power_curve_',analysis_name,'.png'))
     plot(N_vect,
          power_vect_no_age,
          xlab='',
@@ -797,9 +797,9 @@ plot_analysis <- function(analysis_name, N_vect, exp_per_N) {
     points(N_vect, power_vect_age, col='red', type='o')
     axis(1, N_vect, N_vect)
     axis(1, N_vect, round(N_vect/10), line=2.5)
-    x_offset <- 2400
+    x_offset <- 4800
     y_offset <- 1.00
-    mtext("Num Rad Carb Samples", side=1, line=y_offset,       at=2400)
-    mtext("Num Skeletal Samples", side=1, line=y_offset + 2.5, at=2400)
+    mtext("Num Rad Carb Samples", side=1, line=y_offset,       at=x_offset)
+    mtext("Num Skeletal Samples", side=1, line=y_offset + 2.5, at=x_offset)
   dev.off()
 }
