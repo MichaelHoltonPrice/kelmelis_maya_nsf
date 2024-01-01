ffca21326984813c6694c2a83d51b22cc95e0126
MichaelHoltonPrice <MichaelHoltonPrice@gmail.com>
2023-12-31 16:35:50 -0800

    Added age ticks to joint grid

--- make_likelihood_figure.R
+++ make_likelihood_figure.R
@@ -40,29 +40,17 @@ for (i in 1:num_parts) {
 age_marginal <- age_marginal / sum(age_marginal)
 
 ## Create the age_at_death_density plot (rotated 90 degrees and spanning 3.5 to 1.5 on the y-axis)
-y_vect <- seq(3.5, 1.5, length.out = length(age_at_death_density))
-
-# Create breaks for the y-axis
-y_breaks <- c(0, 20, 40, 60, 80)
-# Map these breaks to the corresponding y_vect values
-mapped_y_breaks <- approx(age_groups, y_vect, y_breaks)$y
+x_min <- 0
+x_max <- 1.5
+x <- age_at_death_density
+x_vect <- x_min + ((x - min(x)) * (x_max - x_min)) / (max(x) - min(x))
+y_vect <- seq(3.35, 1.05, length.out = length(age_at_death_density))
 
 age_death_plot <- ggplot() +
-  geom_line(aes(x = age_at_death_density, y = y_vect)) +
+  geom_line(aes(x = x_vect, y = y_vect)) +
+  xlim(x_min, x_max) + ylim(0.5, 4.5) + 
   coord_fixed(ratio = 1) +
-  theme_minimal() +
-  theme(
-    axis.title = element_blank(),  # Remove axis titles
-    axis.text.x = element_blank(),  # Remove x-axis text if not needed
-    axis.ticks.x = element_blank(),  # Remove x-axis ticks if not needed
-    panel.grid = element_blank(),   # Remove grid lines
-    axis.text.y = element_text(angle = -90),  # Rotate y-axis labels by -90 degrees
-    axis.title.y = element_text(angle = -90, size = 12, face = "bold", 
-                                vjust = 0.5),
-                                #vjust = 0.5, margin = margin(t = 0, r = 20, b = 0, l = 0))  # Move y-axis title to the right
-  ) +
-  scale_y_continuous(breaks = mapped_y_breaks, labels = as.character(y_breaks)) +
-  labs(y = "Age [years]")  # Add y-axis label
+  theme_void()
 
 # Make the joint probability from the two marginals
 joint_probability <- outer(pop_size_marginal, rev(age_marginal))
@@ -81,13 +69,13 @@ pop_size_marginal_df <- data.frame(
 
 label_df <- data.frame(
   x = seq(0.5, 4.5, length.out = 5),  # Positions for 5 labels
-  y = rep(5.0, 5),  # Constant y position for all labels
+  y = rep(4.75, 5),  # Constant y position for all labels
   label = seq(600, 1000, by = 100)  # Labels from 600 to 1000
 )
 
 axis_df <- data.frame(
   x = c(2.5),
-  y = c(5.5),
+  y = c(5.25),
   label = 'Calendar Date [AD]'
 )
 
@@ -105,6 +93,17 @@ joint_prob_df <- expand.grid(x = 1:num_parts, y = 1:num_parts)
 joint_prob_df$label <- sprintf("%.2f", as.vector(joint_probability))
 
 # Create a 4x4 grid plot for the joint probability data
+age_label_df <- data.frame(
+  x = rep(4.5, 5),  # Position to the right of the grid
+  y = 0.5:4.5,  # Y positions corresponding to the grid
+  label = c("0", "20", "40", "60", "80")  # Age labels
+)
+
+age_axis_label_df <- data.frame(
+  x = 4.75,  # Position further right from the grid
+  y = 2.5,   # Middle position on the y-axis
+  label = "Age [years]"
+)
 joint_probability_grid_plot <- ggplot(joint_prob_df, aes(x = x, y = y, label = label)) +
   geom_tile(color = "black", fill = "white", size = 0.5) +  # Draw squares with boundaries
   geom_text() +  # Add text labels
@@ -113,6 +112,11 @@ joint_probability_grid_plot <- ggplot(joint_prob_df, aes(x = x, y = y, label = l
   geom_text(data = axis_df, size=6, aes(x = x, y = y, label = label), vjust = 0) +  # Add an axis label
   xlim(0.5, 4.5) + ylim(0.5, 5.5) +  # Set limits to enclose the squares properly
   coord_fixed(ratio = 1)  # Set aspect ratio to 1:1
+joint_probability_grid_plot <- joint_probability_grid_plot +
+    geom_text(data = age_label_df, aes(x = x, y = y, label = label), angle=-90, vjust = -0.5, hjust=0.5, size = 3) 
+    #geom_text(data = age_label_df, aes(x = x, y = y, label = label), angle = -90, vjust = 0.0, hjust = 0, size = 3) 
+#  geom_text(data = age_axis_label_df, aes(x = x, y = y, label = label), vjust = 0.5, size = 6)  # Add age axis label
+
 
 # Create a text plot for cell (1,1)
 text_plot1 <- ggplot() +
@@ -128,23 +132,12 @@ text_plot2 <- ggplot() +
   theme_void() +
   theme(plot.margin = margin(1, 1, 1, 1, "pt"))
 
-# Create a plot with a vertical line
-#vertical_line_plot <- ggplot() + 
-#  geom_vline(xintercept = 0.5, color = "black", size = 0.5) + 
-#  theme_void() +
-#  theme(plot.margin = margin(0, 0, 0, 0)) +
-#  coord_fixed(ratio = 1)
-
 # Creating spacers and empty plots
 empty_plot <- plot_spacer()
 
 # Arrange the plots manually
-widths <- c(2, 1, 2, 1)  # Adjust the width of each column
-heights <- c(1, 1, 4)  # Adjust the height of each row
-#layout <- text_plot1                  + vertical_line_plot + text_plot2                  + empty_plot +
-#          population_plot             + vertical_line_plot + population_plot             + empty_plot +
-#          pop_size_marginal_grid_plot + vertical_line_plot + joint_probability_grid_plot + age_death_plot +
-#          plot_layout(widths = widths, heights = heights)
+widths <- c(2, 1, 2, 2)  # Adjust the width of each column
+heights <- c(1, 1, 2)  # Adjust the height of each row
 layout <- text_plot1                  + empty_plot + text_plot2                  + empty_plot +
           population_plot             + empty_plot + population_plot             + empty_plot +
           pop_size_marginal_grid_plot + empty_plot + joint_probability_grid_plot + age_death_plot +
